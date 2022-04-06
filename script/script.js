23feefa90c8f57c84b4bf4b2135751191089ce49
Ajithvc45 <84002008+Ajithvc45@users.noreply.github.com>
2022-04-06 21:12:23 +0530

    new Git

---
+++ script/script.js
@@ -0,0 +1,32 @@
+// var check = ()=>{
+//     if(document.getElementById("username").value == "admin",
+//     document.getElementById("password").value == 12345){
+//         document.getElementById("error").style.color = "green";
+//         document.getElementById("error").innerHTML = "Done!!";
+//         return true;
+//     }else{
+//         document.getElementById("error").style.color = "red";
+//         document.getElementById("error").innerHTML = "Invalid Username or Password";
+//         return false;
+//     }
+// }
+
+
+var check = (callback)=>{
+    if(document.getElementById("username").value == "admin",
+    document.getElementById("password").value == 12345){
+        callback();
+        document.getElementById("error").style.color = "green";
+        document.getElementById("error").innerHTML = "Done!!";
+        return true;
+    }else{
+        document.getElementById("error").style.color = "red";
+        document.getElementById("error").innerHTML = "Invalid Username or Password";
+        return false;
+    }
+}
+
+function redirect(){
+    document.getElementById("form").action="index.html";
+}
+
