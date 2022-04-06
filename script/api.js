16910f5206c2836dc355b35f894beeb7f2983fc1
Ajithvc45 <84002008+Ajithvc45@users.noreply.github.com>
2022-04-06 22:55:22 +0530

    api

--- script/api.js
+++ script/api.js
@@ -1,21 +1,17 @@
-let todohead = document.getElementById("todohead");
-todohead.addEventListener("click",ajax);
-
 function ajax(){
 
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function(){
-    if(this.readystate==4&&this.status==200){
-        var response = JSON.parse(this.responseText);
-        var output = "";
-        for(var i=0;i<response.length;i++){
-            output += "<li>"+ response[i].title + "</li>";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState==4&&this.status==200){
+            var response = JSON.parse(this.responseText);
+            var output = "";
+            for(var i=0;i<response.length;i++){
+                output += "<li>"+response[i].title;
+            }
+            document.getElementById("demo").innerHTML=output;
         }
-        todohead.innerHTML=output;
     }
-}
-
-xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
-xhttp.send(); 
-
+    
+    xhttp.open("GET","https://jsonplaceholder.typicode.com/todos",true);
+    xhttp.send();
 }
